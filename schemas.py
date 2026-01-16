9e9fa6e004f096ed8b5996def8fce422cc0ea2c2
Mahbub Bello <mahbubbello@gmail.com>
2026-01-16 15:32:17 +0000

    feat: Implement SQLAlchemy ORM for user and post management, migrating from in-memory data to a SQLite database with new API and UI endpoints.

--- schemas.py
+++ schemas.py
@@ -1,14 +1,29 @@
-from pydantic import BaseModel, ConfigDict, Field
+from pydantic import BaseModel, ConfigDict, Field, EmailStr
+from datetime import datetime
+
+class UserBase(BaseModel):
+    username: str = Field(min_length=3, max_length=50)
+    email: EmailStr = Field(min_length=3, max_length=120)
+
+class UserCreate(UserBase):
+    pass
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+    image_file: str | None = None
+    image_path: str
 
 class PostBase(BaseModel):
     title: str = Field(min_length=3, max_length=100)
     content: str = Field(min_length=3, max_length=10000)
-    author: str = Field(min_length=3, max_length=100)
 
 class PostCreate(PostBase):
-    pass
+    user_id: int #temporary
 
 class PostResponse(PostBase):
     model_config = ConfigDict(from_attributes=True)
     id: int
-    date_posted: str
+    user_id: int #temporary
+    date_posted: datetime
+    author: UserResponse
